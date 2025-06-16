8cce6853e9401d58f80b07811331cb986ea19971
KO TAEWON <migmaknoob@gmail.com>
2025-06-16 16:12:31 +0900

    하이브리드 방식으로 변경

--- src/main/frontend/src/main/index.jsx
+++ src/main/frontend/src/main/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // 기존에 사용하던 메인 컴포넌트
-import './index.css';   // 전역 CSS
+import App from './App'; // 라우터가 포함된 App 컴포넌트
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
